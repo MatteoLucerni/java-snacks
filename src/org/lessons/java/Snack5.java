2fc127edd99e207c2bf61701650517bc49d44b72
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-11-10 14:36:23 +0100

    added snack 5 completed

--- src/org/lessons/java/Snack5.java
+++ src/org/lessons/java/Snack5.java
@@ -7,17 +7,36 @@ public class Snack5 {
 		
 		Scanner in = new Scanner(System.in);
 		
-		String userInputString = "";
+		String userString = "";
+		int nNum = 0;
+		int nChars = 0;
+		int nSpecialChars = 0;
 		
 		while(true) {
 			System.out.println("Scrivi qualcosa (0 per interrompere): ");
 	        
-	        userInputString += in.nextLine();
-	        
-	        if (userInputString.contains("0")) break;
-	        System.out.println("La tua password: " + userInputString);
+			String userInput = in.nextLine();
+			if (userInput.equals("0")) break;
+			
+			userString += userInput;
+			
+	        System.out.println("La tua stringa: " + userString);
 		}
 		in.close();
+		
+		for(int i = 0; i < userString.length(); i++) {
+			 char currentChar = userString.charAt(i);
+			 
+			 if(Character.isDigit(currentChar)) {
+				 nNum++;
+			 } else if(Character.isAlphabetic(currentChar)) {
+				 nChars++;
+			 } else {
+				 nSpecialChars++;
+			 }
+		}
+		
+		System.out.println("La tua stringa contiene " + nChars + " lettere " + nNum + " numeri e " + nSpecialChars + " caratteri speciali.");
         
 	}
 }
